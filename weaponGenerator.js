e459455560b7213fd90125a0d5fa8d9f56fa26fc
gpu-maker <32omelada@sandiaprep.org>
2026-02-11 08:36:17 -0700

    Delete weaponGenerator.js

--- weaponGenerator.js
+++
@@ -1,7 +0,0 @@
-export function generateWeapon(){
-  return{
-    name:"Sword "+Math.floor(Math.random()*999),
-    damage:Math.floor(Math.random()*10+5),
-    color:"#"+Math.floor(Math.random()*16777215).toString(16)
-  };
-}
