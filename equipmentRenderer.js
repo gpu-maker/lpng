2001de00bf965b0679de4f1d6e58e8e019dfd70c
gpu-maker <32omelada@sandiaprep.org>
2026-02-11 08:37:16 -0700

    Delete equipmentRenderer.js

--- equipmentRenderer.js
+++
@@ -1,9 +0,0 @@
-export function applyEquipment(base,equipment){
-  if(equipment.helmet)
-    base[0]=base[0].map(()=>equipment.helmet.color);
-
-  if(equipment.chest)
-    base[3]=base[3].map(()=>equipment.chest.color);
-
-  return base;
-}
